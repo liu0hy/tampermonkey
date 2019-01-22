35e2ee66ded3966ea4d58527c7a1f90fec1dc965
liu0hy <liu0hy@gmail.com>
2019-01-22 16:45:55 +0800

    Update format_keys.js

--- format_keys.js
+++ format_keys.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Format keys
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      1.0
 // @description  Format keys to be redeemed by ASF
 // @author       liu0hy
 // @match        https://liu0hy.cn:2096/page/*
@@ -13,18 +13,34 @@
     'use strict';
 
     // Your code here...
-    var e = document.createElement("script");
-    var t = document.createTextNode("function magic() {\n\
-var str = document.getElementsByClassName(\"terminal__input\")[0].value;\n\
-var n = str.match(/\\w{5}-\\w{5}-\\w{5}/g);\n\
-if (!n) {\n\
-console.warn(\"No valid key found\");\n\
-return;\n\
-}\n\
-str = \"redeem master \" + n.join(\",\");\n\
-console.info(str);\n\
-copy(str);\n\
-}");
-    e.appendChild(t);
-    document.body.appendChild(e);
+
+    function magic() {
+        var str = document.getElementsByClassName("terminal__input")[0].value;
+        var n = str.match(/\w{5}-\w{5}-\w{5}/g);
+        if (!n) {
+            alert("No valid key found");
+            return;
+        }
+        var c = "https://liu0hy.cn:2096/api/command/redeem%20master%20" + n.join(",");
+        fetch(c, {method:"POST", headers: {'authentication': 'rqbc6n@ASF'}})
+            .then((res)=>res.json())
+            .then((data)=>{alert(data.Result);});
+    }
+    function prepare_magic() {
+        var f = document.getElementsByClassName("footer__link")[0];
+        var m = f.cloneNode(true);
+        m.innerText = "Magic";
+        m.setAttribute("href", "javascript:void(magic())");
+        f.parentNode.appendChild(m);
+        var s = document.createElement("script");
+        s.innerHTML = magic.toString();
+        document.body.appendChild(s);
+    }
+    var tm = setInterval(function() {
+       var f = document.getElementsByClassName("footer__link")[0];
+       if (f) {
+           prepare_magic();
+           clearInterval(tm);
+       }
+    }, 1000);
 })();
