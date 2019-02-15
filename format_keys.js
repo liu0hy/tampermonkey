aa3f09bb833e695f6a8c84758e6570afd5223ffa
liu0hy <liu0hy@gmail.com>
2019-02-15 14:14:11 +0800

    Update format_keys.js

--- format_keys.js
+++ format_keys.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Format keys
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Format keys to be redeemed by ASF
 // @author       liu0hy
 // @match        https://liu0hy.cn:2096/page/*
@@ -13,34 +13,40 @@
     'use strict';
 
     // Your code here...
-
-    function magic() {
+    function magic(bg) {
         var str = document.getElementsByClassName("terminal__input")[0].value;
         var n = str.match(/\w{5}-\w{5}-\w{5}/g);
         if (!n) {
             alert("No valid key found");
             return;
         }
-        var c = "https://liu0hy.cn:2096/api/command/redeem%20master%20" + n.join(",");
-        fetch(c, {method:"POST", headers: {'authentication': 'rqbc6n@ASF'}})
-            .then((res)=>res.json())
-            .then((data)=>{alert(data.Result);});
+        console.log('redeem master ' + n.join(','));
+        if (bg) {
+            var c = "https://liu0hy.cn:2096/api/command/redeem%20master%20" + n.join(",");
+            fetch(c, {method:"POST", headers: {'authentication': 'rqbc6n@ASF'}})
+                .then((res)=>res.json())
+                .then((data)=>{alert(data.Result);console.log(data.Result);});
+        }
     }
     function prepare_magic() {
         var f = document.getElementsByClassName("footer__link")[0];
         var m = f.cloneNode(true);
-        m.innerText = "Magic";
-        m.setAttribute("href", "javascript:void(magic())");
+        m.innerText = "Yin";
+        m.setAttribute("href", "javascript:void(magic(true))");
+        f.parentNode.appendChild(m);
+        m = f.cloneNode(true);
+        m.innerText = "Yang";
+        m.setAttribute("href", "javascript:void(magic(false))");
         f.parentNode.appendChild(m);
         var s = document.createElement("script");
         s.innerHTML = magic.toString();
         document.body.appendChild(s);
     }
     var tm = setInterval(function() {
-       var f = document.getElementsByClassName("footer__link")[0];
-       if (f) {
-           prepare_magic();
-           clearInterval(tm);
-       }
+        var f = document.getElementsByClassName("footer__link")[0];
+        if (f) {
+            prepare_magic();
+            clearInterval(tm);
+        }
     }, 1000);
 })();
